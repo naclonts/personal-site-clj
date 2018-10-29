4175a29c1b78c1759879b6660981695fe43a4c25
kokopelli314 <nacclonts@gmail.com>
2018-10-28 23:18:55 -0600

    add initial binary tree code

--- src/cljs/personal_site_clj/core.cljs
+++ src/cljs/personal_site_clj/core.cljs
@@ -2,10 +2,66 @@
 	(:require [quil.core :as q :include-macros true]))
 
 (enable-console-print!)
-
 (extend-type js/HTMLCollection
 	ISeqable
 	(-seq [array] (array-seq array 0)))
+
+; Binary Tree goodness
+; thanks to https://eddmann.com/posts/binary-search-trees-in-clojure/
+(defrecord Node [el left right])
+
+(defn tree-min [{:keys [el left]}]
+	(if left
+		(recur left)
+		el))
+
+(defn tree-max [{:keys [el right]}]
+	(if right
+		(recur right)
+		el))
+
+(defn tree-insert [{:keys [el left right] :as tree} value]
+	(cond
+		(nil? tree) (Node. value nil nil)
+		(< value el) (Node. el (tree-insert left value) right)
+		(> value el) (Node. el left (tree-insert right value))
+		:else tree))
+
+(defn tree-remove [{:keys [el left right] :as tree} value]
+	(cond
+		(nil? tree) nil
+		(< value el) (Node. el (tree-remove left value) right)
+		(> value el) (Node. el left (tree-remove right value))
+		(nil? left) right
+		(nil? right) left
+		:else (let [min-value (tree-min right)]
+			(Node. min-value left (tree-remove right min-value)))))
+
+(defn tree-contains? [{:keys [el left right] :as tree} value]
+	(cond
+		(nil? tree) false
+		(< value el) (recur left value)
+		(> value el) (recur right value)
+		:else true))
+
+(defn tree-count [{:keys [left right] :as tree}]
+	(if tree
+		(+ 1 (tree-count left) (tree-count right))
+		0))
+
+(defn tree-height
+	([tree] (tree-height tree 0))
+	([tree count]
+		(if tree
+			(max (tree-height (:left tree) (inc count))
+					 (tree-height (:right tree) (inc count)))
+			count)))
+
+(def to-tree #(reduce tree-insert nil %))
+
+(defn to-list [{:keys [el left right] :as tree}]
+	(when tree
+		`(~@(to-list left) ~el ~@(to-list right))))
 
 ; Event handlers
 (defn trigger-when-clicked [class-name f]
