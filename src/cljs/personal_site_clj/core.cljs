52ac8cf8d7d814ca2ccad4484b1ad54e73afe9ac
kokopelli314 <nacclonts@gmail.com>
2018-10-28 22:16:25 -0600

    pro tip: map evaluates lazily - use doall or something

--- src/cljs/personal_site_clj/core.cljs
+++ src/cljs/personal_site_clj/core.cljs
@@ -3,6 +3,30 @@
 
 (enable-console-print!)
 
+(extend-type js/HTMLCollection
+	ISeqable
+	(-seq [array] (array-seq array 0)))
+
+; Event handlers
+(defn trigger-when-clicked [class-name f]
+	(let [elements (.getElementsByClassName js/document class-name)]
+		(doall (map #(.addEventListener % "click" f) elements))))
+
+(defn toggle-menu [event]
+	(let [hamburger (js/document.getElementById "hamburger")
+				menu (js/document.getElementById "menu")]
+		(.toggle (.-classList menu) "hidden")
+		(.toggle (.-classList hamburger) "is-active")))
+(defn close-menu [event]
+	(let [hamburger (js/document.getElementById "hamburger")
+				menu (js/document.getElementById "menu")]
+		(.add (.-classList menu) "hidden")
+		(.remove (.-classList hamburger) "is-active")))
+
+(trigger-when-clicked "hamburger" toggle-menu)
+(trigger-when-clicked "menu-link" close-menu)
+
+; Animations & drawings
 (defn setup []
 	(q/frame-rate 1)
 	(q/background (q/color 0 0 0 1)))
