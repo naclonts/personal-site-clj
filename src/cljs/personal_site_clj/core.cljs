1673c5d3715d7b38efb13a62ff44e155418c18c1
kokopelli314 <nacclonts@gmail.com>
2018-11-03 15:33:58 -0600

    updated AVL tree implementation

--- src/cljs/personal_site_clj/core.cljs
+++ src/cljs/personal_site_clj/core.cljs
@@ -11,7 +11,7 @@
 ; Binary (AVL) tree implementation
 ; thanks to https://two-wrongs.com/purely-functional-avl-trees-in-common-lisp.html
 
-(deftype AvlTree [key value left right])
+(defrecord AvlTree [key value left right])
 
 (defn tree-height
 	([tree] (tree-height tree 0))
@@ -92,112 +92,15 @@
 		:else (cons value
 						(concat (lookup key left) (lookup key right)))))
 
-
-; ; Binary Tree goodness
-; ; thanks to https://eddmann.com/posts/binary-search-trees-in-clojure/
-; ; and https://eddmann.com/posts/avl-trees-in-clojure/
-; (defrecord Node [el left right])
-
-; (defn tree-min [{:keys [el left]}]
-; 	(if left
-; 		(recur left)
-; 		el))
-
-; (defn tree-max [{:keys [el right]}]
-; 	(if right
-; 		(recur right)
-; 		el))
-
-; (defn tree-insert [{:keys [el left right] :as tree} value]
-; 	(cond
-; 		(nil? tree) (Node. value nil nil)
-; 		(< value el) (Node. el (tree-insert left value) right)
-; 		(> value el) (Node. el left (tree-insert right value))
-; 		:else tree))
-
-; (defn tree-remove [{:keys [el left right] :as tree} value]
-; 	(cond
-; 		(nil? tree) nil
-; 		(< value el) (Node. el (tree-remove left value) right)
-; 		(> value el) (Node. el left (tree-remove right value))
-; 		(nil? left) right
-; 		(nil? right) left
-; 		:else (let [min-value (tree-min right)]
-; 			(Node. min-value left (tree-remove right min-value)))))
-
-; (defn tree-contains? [{:keys [el left right] :as tree} value]
-; 	(cond
-; 		(nil? tree) false
-; 		(< value el) (recur left value)
-; 		(> value el) (recur right value)
-; 		:else true))
-
-; (defn tree-count [{:keys [left right] :as tree}]
-; 	(if tree
-; 		(+ 1 (tree-count left) (tree-count right))
-; 		0))
-
-; (defn tree-height
-; 	([tree] (tree-height tree 0))
-; 	([tree count]
-; 		(if tree
-; 			(max (tree-height (:left tree) (inc count))
-; 					 (tree-height (:right tree) (inc count)))
-; 			count)))
-
-; (def to-tree #(reduce tree-insert nil %))
-
-; (defn to-list [{:keys [el left right] :as tree}]
-; 	(when tree
-; 		`(~@(to-list left) ~el ~@(to-list right))))
-
-; (defn factor [{:keys [left right]}]
-; 	(- (tree-height left) (tree-height right)))
-
-; (defn rotate-left [{:keys [el left right] :as tree}]
-; 	(if right
-; 		(->Node (:el right) (->Node el left (:left right)) (:right right))
-; 		tree))
-
-; (defn rotate-right [{:keys [el left right] :as tree}]
-; 	(if left
-; 		(->Node (:el left) (:left left) (->Node el (:right left) right))
-; 		tree))
-
-; (defn is-left-case? [tree]
-; 	(prn "LL case")
-; 	(< (factor tree) -1))
-; (defn is-left-right-case? [tree]
-; 	(prn "LR case")
-; 	(and (is-left-case? tree) (> (factor (:right tree)) 0)))
-; (defn is-right-case? [tree]
-; 	(prn "RR case")
-; 	(> (factor tree) 1))
-; (defn is-right-left-case? [tree]
-; 	(prn "RL case")
-; 	(and (is-right-case? tree) (< (factor (:left tree)) 0)))
-
-; (defn tree-balance [{:keys [el left right] :as tree}]
-; 	(prn (str "el: " el ", factor: " (factor tree) "\r\n "))
-; 	(cond
-; 		(is-right-left-case? tree) (rotate-right (->Node el (rotate-left left) right))
-; 		(is-left-right-case? tree) (rotate-left (->Node el left (rotate-right right)))
-; 		(is-right-case? tree) (rotate-right tree)
-; 		(is-left-case? tree) (rotate-left tree)
-; 		:else tree))
-
-; (def avl-insert (comp tree-balance tree-insert))
-; (def avl-remove (comp tree-balance tree-remove))
-; (def seq->avl (partial reduce avl-insert nil))
-
 (defn tree-tabs [n]
   (clojure.string/join(repeat n "\t")))
 
-(defn tree-visualise
-  ([tree] (tree-visualise tree 0))
-  ([{:keys [key value left right] :as tree} depth]
+(defn tree-visualize
+  ([tree] (tree-visualize tree 0))
+	([{:keys [key value left right] :as tree} depth]
+		(println (str "key: " key ", value: " value))
     (if tree
-      (str (tree-visualise right (inc depth)) (tree-tabs depth) el "\n" (tree-visualise left (inc depth)))
+      (str (tree-visualize right (inc depth)) (tree-tabs depth) key "\n" (tree-visualize left (inc depth)))
       (str (tree-tabs depth) "~\n"))))
 
 (defn tree-print [tree] (println (tree-visualize tree)))
