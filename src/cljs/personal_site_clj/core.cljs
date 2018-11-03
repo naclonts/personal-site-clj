8426ae71cb772c1d29880e0d8e7bb274f46feedd
kokopelli314 <nacclonts@gmail.com>
2018-11-03 12:17:36 -0600

    initial re-implementation of AVL tree

--- src/cljs/personal_site_clj/core.cljs
+++ src/cljs/personal_site_clj/core.cljs
@@ -7,114 +7,206 @@
 	ISeqable
 	(-seq [array] (array-seq array 0)))
 
-; Binary Tree goodness
-; thanks to https://eddmann.com/posts/binary-search-trees-in-clojure/
-; and https://eddmann.com/posts/avl-trees-in-clojure/
-(defrecord Node [el left right])
 
-(defn tree-min [{:keys [el left]}]
-	(if left
-		(recur left)
-		el))
+; Binary (AVL) tree implementation
+; thanks to https://two-wrongs.com/purely-functional-avl-trees-in-common-lisp.html
 
-(defn tree-max [{:keys [el right]}]
-	(if right
-		(recur right)
-		el))
-
-(defn tree-insert [{:keys [el left right] :as tree} value]
-	(cond
-		(nil? tree) (Node. value nil nil)
-		(< value el) (Node. el (tree-insert left value) right)
-		(> value el) (Node. el left (tree-insert right value))
-		:else tree))
-
-(defn tree-remove [{:keys [el left right] :as tree} value]
-	(cond
-		(nil? tree) nil
-		(< value el) (Node. el (tree-remove left value) right)
-		(> value el) (Node. el left (tree-remove right value))
-		(nil? left) right
-		(nil? right) left
-		:else (let [min-value (tree-min right)]
-			(Node. min-value left (tree-remove right min-value)))))
-
-(defn tree-contains? [{:keys [el left right] :as tree} value]
-	(cond
-		(nil? tree) false
-		(< value el) (recur left value)
-		(> value el) (recur right value)
-		:else true))
-
-(defn tree-count [{:keys [left right] :as tree}]
-	(if tree
-		(+ 1 (tree-count left) (tree-count right))
-		0))
+(deftype AvlTree [key value left right])
 
 (defn tree-height
 	([tree] (tree-height tree 0))
-	([tree count]
-		(if tree
-			(max (tree-height (:left tree) (inc count))
-					 (tree-height (:right tree) (inc count)))
-			count)))
+	([tree height] 
+		(if (nil? tree)
+			height
+			(max (tree-height (.-left tree) (inc height))
+					 (tree-height (.-right tree) (inc height))))))
 
-(def to-tree #(reduce tree-insert nil %))
+(defn balance-factor [node]
+	"Get the balance factor of subtree rooted at node."
+	(case (- (tree-height (.-right node))
+					 (tree-height (.-left node)))
+		-2 :imbalanced-left
+		-1 :left-heavy
+		 0 :balanced
+		+1 :right-heavy
+		+2 :imbalanced-right))
 
-(defn to-list [{:keys [el left right] :as tree}]
-	(when tree
-		`(~@(to-list left) ~el ~@(to-list right))))
+(defn rotate-left [node]
+	"Return tree rotated left."
+	(let [{:keys [key value left right]} node
+				height (tree-height node)]
+		(avl-node
+			(.-key right)
+			(.-value right)
+			(avl-node key value left (.-left right))
+			(.-right right))))
 
-(defn factor [{:keys [left right]}]
-	(- (tree-height left) (tree-height right)))
+(defn rotate-right [node]
+	"Return tree rotated right."
+	(let [{:keys [key value left right]} node
+				height (tree-height node)]
+		(avl-node
+			(.-key left)
+			(.-value left)
+			(.-left left)
+			(avl-node key value (.-right left) right))))
 
-(defn rotate-left [{:keys [el left right] :as tree}]
-	(if right
-		(->Node (:el right) (->Node el left (:left right)) (:right right))
-		tree))
+(defn avl-node [key value left right]
+	(let [node (AvlTree. key value left right)]
+		(case (balance-factor node)
+			(:left-heavy :balanced :right-heavy)
+				node
 
-(defn rotate-right [{:keys [el left right] :as tree}]
-	(if left
-		(->Node (:el left) (:left left) (->Node el (:right left) right))
-		tree))
+			:imbalanced-left
+				(case (balance-factor left)
+					:left-heavy
+						(rotate-right node)
+					:right-heavy
+						(avl-node key value (rotate-left left) right))
 
-(defn is-left-case? [tree]
-	(prn "LL case")
-	(< (factor tree) -1))
-(defn is-left-right-case? [tree]
-	(prn "LR case")
-	(and (is-left-case? tree) (> (factor (:right tree)) 0)))
-(defn is-right-case? [tree]
-	(prn "RR case")
-	(> (factor tree) 1))
-(defn is-right-left-case? [tree]
-	(prn "RL case")
-	(and (is-right-case? tree) (< (factor (:left tree)) 0)))
+			:imbalanced-right
+				(case (balance-factor right)
+					:left-heavy
+						(avl-node key value left (rotate-right right))
+					:right-heavy
+						(rotate-left node)))))
 
-(defn tree-balance [{:keys [el left right] :as tree}]
-	(prn (str "el: " el ", factor: " (factor tree) "\r\n "))
+(defn avl-insert [key value tree]
+	(if (nil? tree)
+		(avl-node key value nil nil)
+		(avl-node
+			(.-key tree)
+			(.-value tree)
+			(if (< key (.-key tree))
+				(avl-insert key value (.-left tree))
+				(.-left tree))
+			(if (< key (.-key tree))
+				(.-right tree)
+				(avl-insert key value (.-right tree))))))
+
+(defn lookup [key {:keys [node-key value left right] :as tree}]
+	"Returns all values associated with key in tree."
 	(cond
-		(is-right-left-case? tree) (rotate-right (->Node el (rotate-left left) right))
-		(is-left-right-case? tree) (rotate-left (->Node el left (rotate-right right)))
-		(is-right-case? tree) (rotate-right tree)
-		(is-left-case? tree) (rotate-left tree)
-		:else tree))
+		(< key node-key) (lookup key left)
+		(< node-key key) (lookup key right)
+		:else (cons value
+						(concat (lookup key left) (lookup key right)))))
 
-(def avl-insert (comp tree-balance tree-insert))
-(def avl-remove (comp tree-balance tree-remove))
-(def seq->avl (partial reduce avl-insert nil))
+
+; ; Binary Tree goodness
+; ; thanks to https://eddmann.com/posts/binary-search-trees-in-clojure/
+; ; and https://eddmann.com/posts/avl-trees-in-clojure/
+; (defrecord Node [el left right])
+
+; (defn tree-min [{:keys [el left]}]
+; 	(if left
+; 		(recur left)
+; 		el))
+
+; (defn tree-max [{:keys [el right]}]
+; 	(if right
+; 		(recur right)
+; 		el))
+
+; (defn tree-insert [{:keys [el left right] :as tree} value]
+; 	(cond
+; 		(nil? tree) (Node. value nil nil)
+; 		(< value el) (Node. el (tree-insert left value) right)
+; 		(> value el) (Node. el left (tree-insert right value))
+; 		:else tree))
+
+; (defn tree-remove [{:keys [el left right] :as tree} value]
+; 	(cond
+; 		(nil? tree) nil
+; 		(< value el) (Node. el (tree-remove left value) right)
+; 		(> value el) (Node. el left (tree-remove right value))
+; 		(nil? left) right
+; 		(nil? right) left
+; 		:else (let [min-value (tree-min right)]
+; 			(Node. min-value left (tree-remove right min-value)))))
+
+; (defn tree-contains? [{:keys [el left right] :as tree} value]
+; 	(cond
+; 		(nil? tree) false
+; 		(< value el) (recur left value)
+; 		(> value el) (recur right value)
+; 		:else true))
+
+; (defn tree-count [{:keys [left right] :as tree}]
+; 	(if tree
+; 		(+ 1 (tree-count left) (tree-count right))
+; 		0))
+
+; (defn tree-height
+; 	([tree] (tree-height tree 0))
+; 	([tree count]
+; 		(if tree
+; 			(max (tree-height (:left tree) (inc count))
+; 					 (tree-height (:right tree) (inc count)))
+; 			count)))
+
+; (def to-tree #(reduce tree-insert nil %))
+
+; (defn to-list [{:keys [el left right] :as tree}]
+; 	(when tree
+; 		`(~@(to-list left) ~el ~@(to-list right))))
+
+; (defn factor [{:keys [left right]}]
+; 	(- (tree-height left) (tree-height right)))
+
+; (defn rotate-left [{:keys [el left right] :as tree}]
+; 	(if right
+; 		(->Node (:el right) (->Node el left (:left right)) (:right right))
+; 		tree))
+
+; (defn rotate-right [{:keys [el left right] :as tree}]
+; 	(if left
+; 		(->Node (:el left) (:left left) (->Node el (:right left) right))
+; 		tree))
+
+; (defn is-left-case? [tree]
+; 	(prn "LL case")
+; 	(< (factor tree) -1))
+; (defn is-left-right-case? [tree]
+; 	(prn "LR case")
+; 	(and (is-left-case? tree) (> (factor (:right tree)) 0)))
+; (defn is-right-case? [tree]
+; 	(prn "RR case")
+; 	(> (factor tree) 1))
+; (defn is-right-left-case? [tree]
+; 	(prn "RL case")
+; 	(and (is-right-case? tree) (< (factor (:left tree)) 0)))
+
+; (defn tree-balance [{:keys [el left right] :as tree}]
+; 	(prn (str "el: " el ", factor: " (factor tree) "\r\n "))
+; 	(cond
+; 		(is-right-left-case? tree) (rotate-right (->Node el (rotate-left left) right))
+; 		(is-left-right-case? tree) (rotate-left (->Node el left (rotate-right right)))
+; 		(is-right-case? tree) (rotate-right tree)
+; 		(is-left-case? tree) (rotate-left tree)
+; 		:else tree))
+
+; (def avl-insert (comp tree-balance tree-insert))
+; (def avl-remove (comp tree-balance tree-remove))
+; (def seq->avl (partial reduce avl-insert nil))
 
 (defn tree-tabs [n]
   (clojure.string/join(repeat n "\t")))
 
 (defn tree-visualise
   ([tree] (tree-visualise tree 0))
-  ([{:keys [el left right] :as tree} depth]
+  ([{:keys [key value left right] :as tree} depth]
     (if tree
       (str (tree-visualise right (inc depth)) (tree-tabs depth) el "\n" (tree-visualise left (inc depth)))
       (str (tree-tabs depth) "~\n"))))
 
-; Event handlers
+(defn tree-print [tree] (println (tree-visualize tree)))
+
+
+
+
+
+Event handlers
 (defn trigger-when-class-clicked [class-name f]
 	(let [elements (.getElementsByClassName js/document class-name)]
 		(doall (map #(.addEventListener % "click" f) elements))))
@@ -133,61 +225,61 @@
 (trigger-when-class-clicked "hamburger" toggle-menu)
 (trigger-when-class-clicked "menu-link" close-menu)
 
-; Animations & drawings
+; ; Animations & drawings
 
-(def tree-settings
-	{:x-spread 200
-	 :node-r 20
-	 :level-height 50})
+; (def tree-settings
+; 	{:x-spread 200
+; 	 :node-r 20
+; 	 :level-height 50})
 
-(defn setup []
-	(do
-		(q/frame-rate 1)
-		(q/background (q/color 0 0 0 1))
-		(let [tree (seq->avl '(1 2 3 4 5 6 7 8 9))]
-			(prn tree)
-			(tree-visualise tree)
-			; initial state
-			{:x 400 :y 50 :tree tree})))
+; (defn setup []
+; 	(do
+; 		(q/frame-rate 1)
+; 		(q/background (q/color 0 0 0 1))
+; 		(let [tree (seq->avl '(1 2 3 4 5 6 7 8 9))]
+; 			(prn tree)
+; 			(tree-visualise tree)
+; 			; initial state
+; 			{:x 400 :y 50 :tree tree})))
 
-(defn update-circle [state]
-	(update-in state [:r] inc))
+; (defn update-circle [state]
+; 	(update-in state [:r] inc))
 
-(defn draw-tree
-	([tree x y]
-		(draw-tree tree x y 0))
-	([tree x base-y depth]
-		(let [y (+ base-y (:level-height tree-settings))
-					r (:node-r tree-settings)
-					x-delta (/ (:x-spread tree-settings) (Math/pow 2 (inc depth)))]
-			(q/ellipse x y r r)
-			(q/text (:el tree) (+ x 10) y)
-			(if (nil? (:left tree))
-				nil
-				(draw-tree (:left tree) (- x x-delta) y (inc depth)))
-			(if (nil? (:right tree))
-				nil
-				(draw-tree (:right tree) (+ x x-delta) y (inc depth))))))
+; (defn draw-tree
+; 	([tree x y]
+; 		(draw-tree tree x y 0))
+; 	([tree x base-y depth]
+; 		(let [y (+ base-y (:level-height tree-settings))
+; 					r (:node-r tree-settings)
+; 					x-delta (/ (:x-spread tree-settings) (Math/pow 2 (inc depth)))]
+; 			(q/ellipse x y r r)
+; 			(q/text (:el tree) (+ x 10) y)
+; 			(if (nil? (:left tree))
+; 				nil
+; 				(draw-tree (:left tree) (- x x-delta) y (inc depth)))
+; 			(if (nil? (:right tree))
+; 				nil
+; 				(draw-tree (:right tree) (+ x x-delta) y (inc depth))))))
 
 
-(defn draw [state]
-	; traverse & draw the tree
-	(draw-tree (:tree state) (:x state) (:y state)))
+; (defn draw [state]
+; 	; traverse & draw the tree
+; 	(draw-tree (:tree state) (:x state) (:y state)))
 
-; (defn shrink [r]
-; 	(max min-r (- r 3)))
+; ; (defn shrink [r]
+; ; 	(max min-r (- r 3)))
 
-; (defn mouse-moved [state event]
-; 	(-> state
-; 		(assoc :x (:x event) :y (:y event))
-; 		(update-in [:r] shrink)))
+; ; (defn mouse-moved [state event]
+; ; 	(-> state
+; ; 		(assoc :x (:x event) :y (:y event))
+; ; 		(update-in [:r] shrink)))
 
-(q/defsketch fun-mode-times
-	:host "test-canvas"
-	:size [(- (.-innerWidth js/window) 25) 500]
-	:setup setup
-	:draw draw
-	:update update-circle
-	:middleware [m/fun-mode])
+; (q/defsketch fun-mode-times
+; 	:host "test-canvas"
+; 	:size [(- (.-innerWidth js/window) 25) 500]
+; 	:setup setup
+; 	:draw draw
+; 	:update update-circle
+; 	:middleware [m/fun-mode])
 
 (defn render [] nil)
