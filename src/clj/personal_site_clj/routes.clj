53df63bfad8dd0f47485e811cddef04a7d1b742d
kokopelli314 <nacclonts@gmail.com>
2018-11-10 13:35:56 -0700

    redirect after receipt

--- src/clj/personal_site_clj/routes.clj
+++ src/clj/personal_site_clj/routes.clj
@@ -2,7 +2,7 @@
   (:require [clojure.java.io :as io]
             [compojure.core :refer [ANY GET PUT POST DELETE routes]]
             [compojure.route :refer [resources]]
-						[ring.util.response :refer [response]]
+						[ring.util.response :refer [response redirect]]
 						[environ.core :refer [env]]
 						[personal-site-clj.mail :as mail]))
 
@@ -16,5 +16,8 @@
 					(assoc :headers { "Content-Type" "text/html; charset=utf-8" })))
 		(POST "/contact-form" req
 			(mail/contact-submission (req :params))
-			(str req (env :email-host-user)))
-		(resources "/")))
+			(redirect "/#contact"))
+		(resources "/")
+		; @TODO: add 404 page.
+		(ANY "/*" [path]
+			(redirect "/"))))
