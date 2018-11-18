628135b18275a28f5fcd564cfea65561494a9273
kokopelli314 <nacclonts@gmail.com>
2018-11-17 22:18:01 -0700

    initial cities graph construction

--- src/cljs/personal_site_clj/cities_graph.cljs
+++ src/cljs/personal_site_clj/cities_graph.cljs
@@ -6,11 +6,13 @@
              :as async
              :refer [>! <! go chan close!]]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Utility functions
 (defn log [x]
   (.log js/console x))
 
 (defn get-cities-data []
-  "Returns array of city data map objects."
+  "Returns channel that gives the array of city data map objects."
   (let [out (chan)]
     (let [handler
           (fn [res] (go (log res) (>! out res) (close! out)))]
@@ -20,15 +22,41 @@
             :keywords? true}))
     out))
 
-; Graph structure implementation
+(defn distance-between-points
+  "Returns miles between two points on the earth."
+  [lat1 lon1 lat2 lon2]
+  (let [r 6371e3 ;; earth's radius in meters
+        p1 (q/radians lat1)
+        p2 (q/radians lat2)
+        delta-p (q/radians (- lat2 lat1))
+        delta-l (q/radians (- lon2 lon1))]
+    (let [a (+
+             (q/sq (q/sin (/ delta-p 2)))
+             (* (q/cos p1) (q/cos p2)
+                (q/sq (q/sin (/ delta-l 2)))))
+          c (* 2 (q/atan2 (q/sqrt a) (q/sqrt (- 1 a))))]
+      ;; calculate and convert to miles
+      (* r c 0.0006214))))
+
+(defn distance-between-cities
+  "Returns miles between two city maps."
+  [city1 city2]
+  (distance-between-points
+   (:latitude city1) (:longitude city1)
+   (:latitude city2) (:longitude city2)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Graph structure implementation
 (defrecord Graph [vertices])
 (defrecord Vertex
-    ;; "Vertex structure:
+    ;; Vertex structure:
 		;; 	- key : unique string
 		;; 	- value : payload
-		;; 	- connections : vector of keys to connected vertices
-		;; 	- state : search state keyword
-		;; 	- parent : key of parent vertex"
+		;; 	- connections : set of keys to connected vertices
+		;; 	- state : search state (undiscovered, discovered,
+    ;;            or completely-explored)
+		;; 	- parent : key of parent vertex
     [key value connections state parent])
 
 (defn add-vertex
@@ -76,17 +104,51 @@
       (go (>! out v)))
     out))
 
+(defn connect-cities?
+  [city1 city2]
+  (< (distance-between-cities city1 city2) 600))
+
+(defn add-city
+  "Adds a city to `g` as a vertex, including creating connections
+  to neighbors."
+  [city {:keys [vertices] :as graph}]
+  (println (str "city = " city))
+  (let [g (add-vertex (:city city) city graph)
+        v (get-vertex (:city city) g)
+        vertex-list (into () (seq vertices))]
+    (loop [existing-vertices (rest vertex-list)
+           [key u] (peek vertex-list)
+           new-graph g]
+      (if (nil? u)
+        new-graph
+        (if (connect-cities? city (:value u))
+          (recur
+           (rest existing-vertices) (peek existing-vertices)
+           (add-edge v u new-graph))
+          (recur
+           (rest existing-vertices) (peek existing-vertices)
+           new-graph))))))
+
+(defn build-cities-graph
+  "Transforms the vector of cities data into a graph."
+  [data]
+  (println data)
+  (loop [graph (->Graph {})
+         data (into () data)]
+    (let [city (peek data)]
+      (if (nil? city)
+        graph
+        (recur
+         (add-city city graph)
+         (rest data))))))
+
 (def initial-data (atom {}))
 (defn cities-setup []
   (q/background (q/color 0 30 70 100))
   (q/frame-rate 1)
-  (log "past Go")
-  (println @initial-data)
-  (as-> (->Graph {}) g
-    (add-vertex "hi" 42 (add-vertex "bye" 55 g))
-    (add-vertex "sup" 11 g)
-    (add-edge (get-vertex "hi" g) (get-vertex "bye" g) g)
-    (add-edge (get-vertex "sup" g) (get-vertex "hi" g) g)
+  (let [g (build-cities-graph @initial-data)]
+    (println "graph built:")
+    (println g)
     {:graph g :vertex-explorer (traverse g)}))
 
 (defn cities-update [{:keys [graph vertex-explorer] :as state}]
@@ -109,5 +171,6 @@
     :draw cities-draw
     :middleware [m/fun-mode]))
 
+;; Start the sketch once the JSON is loaded in
 (go (start (<! (get-cities-data))))
 
